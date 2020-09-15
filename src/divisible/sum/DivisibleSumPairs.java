1f9ffe6741d0ed06c6163c92f4f40b83f8d68f6e
sjamloki <sjamloki@in.ibm.com>
2020-09-15 12:27:38 +0530

    fix compile issue

--- src/divisible/sum/DivisibleSumPairs.java
+++ src/divisible/sum/DivisibleSumPairs.java
@@ -3,7 +3,7 @@ package divisible.sum;
 public class DivisibleSumPairs {
 	// Complete the divisibleSumPairs function below.
     static int divisibleSumPairs(int n, int k, int[] ar) {
-    	int result =0;
+     	int result =0;
     	for (int i = 0; i < ar.length; i++) {
 			
     		for(int j=i+1; j < ar.length;j++)
@@ -15,7 +15,11 @@ public class DivisibleSumPairs {
 			}
 		}
     	return result;
-    }
+    } 
+    	/*long result[] = new long[] {0};
+    	IntStream.range(0,n).forEach(i->{
+    		result[0] = IntStream.range(i,n).filter(j->(ar[i]+ar[j])%k ==0).count();
+    		}); */
     
 	public static void main(String[] args) {
 		int [] ar = new int[] {1,3,2,6,1,2};
